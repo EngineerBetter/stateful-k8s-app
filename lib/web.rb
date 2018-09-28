927f7c51b3d2928d871e49611437b9cee2b1bd11
Will Gant <will.gant@engineerbetter.com>
2018-09-28 17:22:10 +0100

    Stateful app works without a db
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- lib/web.rb
+++ lib/web.rb
@@ -6,13 +6,16 @@ require "json"
 require "ostruct"
 require "securerandom"
 
-REDIS = Redis::Namespace.new("k8s-app", redis: Redis.new(
-  host: ENV.fetch("REDIS_SERVICE_SERVICE_HOST"),
-  password: nil,
-  port: ENV.fetch("REDIS_SERVICE_SERVICE_PORT", 6379)
-))
+unless ENV['REDIS_SERVICE_SERVICE_HOST'].nil?
+  REDIS = Redis::Namespace.new("k8s-app", redis: Redis.new(
+    host: ENV.fetch("REDIS_SERVICE_SERVICE_HOST"),
+    password: nil,
+    port: ENV.fetch("REDIS_SERVICE_SERVICE_PORT", 6379)
+  ))
+end
 
 class Web < Sinatra::Base
+  enable :sessions
   helpers do
     def addr
       @addr =
@@ -24,8 +27,21 @@ class Web < Sinatra::Base
   end
 
   get "/" do
-    @total_instance_responses = REDIS.incr("total_instance_#{addr}_responses")
-    @total_app_responses = REDIS.incr("total_app_responses")
+    if ENV['REDIS_SERVICE_SERVICE_HOST'].nil?
+      if session[:total_instance_responses].nil?
+        session[:total_instance_responses] = 0
+      end
+      if session[:total_app_responses].nil?
+        session[:total_app_responses] = 0
+      end
+      @total_instance_responses = session[:total_instance_responses] + 1
+      @total_app_responses = session[:total_app_responses] + 1
+      session[:total_instance_responses] = @total_instance_responses
+      session[:total_app_responses] = @total_app_responses
+    else
+      @total_instance_responses = REDIS.incr("total_instance_#{addr}_responses")
+      @total_app_responses = REDIS.incr("total_app_responses")
+    end
     erb :index
   end
 
