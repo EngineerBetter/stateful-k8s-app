130e8952ad6f1b3c1734ea7b034eb5eef4c1c9f2
Colin Simmons <colin.simmons@engineerbetter.com>
2018-09-28 17:43:00 +0100

    lint web.rb

--- lib/web.rb
+++ lib/web.rb
@@ -1,48 +1,45 @@
-require "sinatra/base"
-require "redis"
-require "redis-namespace"
+# frozen_string_literal: true
 
-require "json"
-require "ostruct"
-require "securerandom"
+require 'sinatra/base'
+require 'redis'
+require 'redis-namespace'
+
+require 'json'
+require 'ostruct'
+require 'securerandom'
 
 unless ENV['REDIS_SERVICE_SERVICE_HOST'].nil?
-  REDIS = Redis::Namespace.new("k8s-app", redis: Redis.new(
-    host: ENV.fetch("REDIS_SERVICE_SERVICE_HOST"),
+  REDIS = Redis::Namespace.new('k8s-app', redis: Redis.new(
+    host: ENV.fetch('REDIS_SERVICE_SERVICE_HOST'),
     password: nil,
-    port: ENV.fetch("REDIS_SERVICE_SERVICE_PORT", 6379)
+    port: ENV.fetch('REDIS_SERVICE_SERVICE_PORT', 6379)
   ))
 end
 
 class Web < Sinatra::Base
-  enable :sessions
   helpers do
     def addr
       @addr =
-        [
-          env.fetch("SERVER_NAME", "127.0.0.1"),
-          env.fetch("SERVER_PORT", "*"),
-        ].join(":")
+      [
+        env.fetch('SERVER_NAME', '127.0.0.1'),
+        env.fetch('SERVER_PORT', '*')
+      ].join(':')
     end
   end
 
-  get "/" do
+  enable :sessions if ENV['REDIS_SERVICE_SERVICE_HOST'].nil?
+  get '/' do
     if ENV['REDIS_SERVICE_SERVICE_HOST'].nil?
-      if session[:total_instance_responses].nil?
-        session[:total_instance_responses] = 0
-      end
-      if session[:total_app_responses].nil?
-        session[:total_app_responses] = 0
-      end
-      @total_instance_responses = session[:total_instance_responses] + 1
-      @total_app_responses = session[:total_app_responses] + 1
-      session[:total_instance_responses] = @total_instance_responses
-      session[:total_app_responses] = @total_app_responses
+      session[:instance_responses] = 0 if session[:instance_responses].nil?
+      session[:app_responses] = 0 if session[:app_responses].nil?
+      @total_instance_responses = session[:instance_responses] + 1
+      @total_app_responses = session[:app_responses] + 1
+      session[:instance_responses] = @total_instance_responses
+      session[:app_responses] = @total_app_responses
     else
       @total_instance_responses = REDIS.incr("total_instance_#{addr}_responses")
-      @total_app_responses = REDIS.incr("total_app_responses")
+      @total_app_responses = REDIS.incr('total_app_responses')
     end
     erb :index
   end
-
 end
