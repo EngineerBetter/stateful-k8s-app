bfd3e556cdaa638cf5a3a059d362f695f6bdc2c2
Sapphire Mason-Brown <sapphire.mason-brown@engineerbetter.com>
2018-10-04 12:35:22 +0100

    Students know if Redis is running if it is in fact running

--- lib/web.rb
+++ lib/web.rb
@@ -37,22 +37,27 @@ class Web < Sinatra::Base
   end
 
   enable :sessions
+
   get '/' do
     session[:instance_responses] = 0 if session[:instance_responses].nil?
     @total_instance_responses = session[:instance_responses] + 1
     session[:instance_responses] = @total_instance_responses
     if ENV['REDIS_SERVICE_SERVICE_HOST'].nil?
+      session[:instance_responses] = "" if session[:instance_responses].nil?
+      @redis_state = session[:redis_state]
       session[:app_responses] = 0 if session[:app_responses].nil?
       @total_app_responses = session[:app_responses] + 1
       session[:app_responses] = @total_app_responses
     else
+      session[:instance_responses] = "" if session[:instance_responses].nil?
+      session[:redis_state] = "Connected to a Redis instance at #{ENV['REDIS_SERVICE_SERVICE_HOST']}"
+      @redis_state = session[:redis_state]
       @total_app_responses = REDIS.incr('total_app_responses')
     end
     erb :index
   end
 
   get '/crash' do
-    REDIS.set("total_instance_#{addr}_responses", 0, :xx => true) if ENV['REDIS_SERVICE_SERVICE_HOST']
     stop.call
     %(
       <h2>Oh no! I've crashed!</h2>
